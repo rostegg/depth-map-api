58afe998d6348c2b89906b3560299d60ce9b1ce6
rostegg <rostik.nikolaenko@gmail.com>
2019-04-04 21:40:49 +0300

    Silente mode for wget

--- get_models_monodepth.sh
+++ get_models_monodepth.sh
@@ -9,7 +9,7 @@ do
     OUTPUT_DIR="$INSTALL_SCRIPT_DIR/app/cnns/monodepth/models"
     OUTPUT_FILE="$OUTPUT_DIR/model_$MODEL.zip"
     echo -e "Downloading \033[0;32mmodel_${MODEL}.zip\033[0m in \033[0;32m${OUTPUT_DIR}\033[0m"
-    wget -nc $URL -O $OUTPUT_FILE
+    wget -q -nc $URL -O $OUTPUT_FILE
     unzip $OUTPUT_FILE -d $OUTPUT_DIR
     rm $OUTPUT_FILE
 done
